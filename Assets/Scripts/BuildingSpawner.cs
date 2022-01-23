23fe1c1ec66e8004bc09c920f346d679a6befec0
Hetarth Thakor <GD04Hetarth@thecea.ca>
2022-01-22 18:47:11 -0800

    Added Score updater

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -1,22 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingSpawner : MonoBehaviour
 {
-    
+
     public float snapValue = 20;
 
     public GameObject rabbit;
 
     public LayerMask actor;
 
+    public int rabbitCost = 2;
+
+    public Text costText;
+
+    void Start()
+    {
+        costText.text = "Rabbit Cost: " + rabbitCost;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+
         Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero, Mathf.Infinity, actor);
         if (hit)
@@ -28,7 +34,9 @@ public class BuildingSpawner : MonoBehaviour
         if (Input.GetKeyDown("space"))
         {
             Instantiate(rabbit, new Vector2(mousePoint.x, mousePoint.y), Quaternion.identity);
-            
+            rabbitCost += 2;
         }
+
+        costText.text = "Rabbit Cost: " + rabbitCost;
     }
 }
